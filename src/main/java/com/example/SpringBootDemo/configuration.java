04e90efd4835befd93b59a3b060bdd5d6a11b9d5
WizkWong <chijianwong7@gmail.com>
2023-04-07 19:44:26 +0800

    add env file and configure CORS

--- src/main/java/com/example/SpringBootDemo/configuration.java
+++ src/main/java/com/example/SpringBootDemo/configuration.java
@@ -6,11 +6,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
 
 @Configuration
 public class configuration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
     @Bean
     CommandLineRunner commandLineRunner(ProductRepository productRepository) {
