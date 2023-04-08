d7d29270bdcf3383fb1b672ee02e6564930eec98
WizkWong <chijianwong7@gmail.com>
2023-04-08 18:10:08 +0800

    create router path

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,26 @@
-
+import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignUp from "./pages/SignUp/SignUp";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const location = useLocation();
+  const excludedPaths = ["/signup"];
+
+  // check if the current path is in the excludedPaths list
+  const shouldDisplayNavbar = !excludedPaths.includes(location.pathname);
+
   return (
-    <div className="App">
-      <h1 className="text-4xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
+    <>
+        {shouldDisplayNavbar && <Navbar />}
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+        </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/frontend/src/pages/SignUp/SignUpForm.tsx
+++ src/frontend/src/pages/SignUp/SignUpForm.tsx
@@ -4,15 +4,15 @@ import FormInput from "../../components/FormInput";
 const SignUpForm = ({ form, bttAction }: { form: FormAttribute[], bttAction: (e: React.MouseEvent<HTMLElement>) => void }) => {
   return (
     <div className="max-w-2xl mx-auto my-4 p-8 shadow border-b">
-    <h1 className="font-bold text-3xl tracking-wider">Sign Up</h1>
-    <FormInput form={form} />
-    <button
-      onClick={bttAction}
-      className="rounded text-white font-semibold bg-cyan-600 px-3 py-2"
-    >
-      Sign Up
-    </button>
-  </div>
+      <h1 className="font-bold text-3xl tracking-wider">Sign Up</h1>
+      <FormInput form={form} />
+      <button
+        onClick={bttAction}
+        className="rounded text-white font-semibold bg-cyan-600 px-3 py-2"
+      >
+        Sign Up
+      </button>
+    </div>
   )
 }
 
