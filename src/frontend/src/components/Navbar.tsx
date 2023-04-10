c31528c65e1e1dd5c593d1664a7a0558a97286ad
WizkWong <chijianwong7@gmail.com>
2023-04-10 15:30:16 +0800

    add navigation link

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import search from "../assets/search.svg";
 
 const Navbar = () => {
@@ -7,26 +8,29 @@ const Navbar = () => {
     "Accessories",
     "Technology",
   ];
+
+  const navigate = useNavigate();
+
   return (
     <nav>
       <div className="flex items-center bg-gray-300 py-3 text-xl">
-        <div className="flex-none mx-4">E-Commerce</div>
+        <div className="flex-none mx-4">{import.meta.env.VITE_WEBSITE_TITLE}</div>
         <div className="flex-1 flex item-center">
           <input className="grow pl-2 py-2" placeholder="Search ..."></input>
           <img
             src={search}
             alt="search"
-            className="flex-none bg-yellow-600 px-3"
+            className="flex-none bg-yellow-600 px-3 hover:cursor-pointer"
           ></img>
         </div>
         <div className="flex-none flex items-center">
-          <p className="px-6">Login</p>
-          <p className="px-6">Sign Up</p>
+          <p className="px-6 hover:cursor-pointer" onClick={() => navigate("/login")}>Login</p>
+          <p className="px-6 hover:cursor-pointer" onClick={() => navigate("/signup")}>Sign Up</p>
         </div>
       </div>
       <div className="flex items-center bg-gray-100 py-1">
         {nav_category.map((element) => (
-          <p className="px-8 hover:text-black text-gray-600 font-bold">
+          <p className="px-8 hover:text-black hover:cursor-pointer text-gray-600 font-bold">
             {element}
           </p>
         ))}
--- src/frontend/src/pages/SignUp/SignUpForm.tsx
+++ src/frontend/src/pages/SignUp/SignUpForm.tsx
@@ -1,18 +1,28 @@
 import { FormAttribute } from "../../types/UserInterfaces"
 import FormInput from "../../components/FormInput";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = ({ form, btnAction }: { form: FormAttribute[], btnAction: (e: React.MouseEvent<HTMLElement>) => void }) => {
+  const navigate = useNavigate();
   return (
-    <div className="max-w-2xl mx-auto my-4 p-8 shadow border-b">
-      <h1 className="font-bold text-3xl tracking-wider">Sign Up</h1>
-      <FormInput form={form} />
-      <button
-        onClick={btnAction}
-        className="rounded text-white font-semibold bg-cyan-600 px-3 py-2"
+    <>
+      <h1
+        className="ml-10 mt-5 font-bold text-2xl tracking-wider hover:cursor-pointer"
+        onClick={() => navigate("/")}
       >
-        Sign Up
-      </button>
-    </div>
+        {import.meta.env.VITE_WEBSITE_TITLE}
+      </h1>
+      <div className="max-w-2xl mx-auto my-4 p-8 shadow border-b">
+        <h1 className="font-bold text-3xl tracking-wider">Sign Up</h1>
+        <FormInput form={form} />
+        <button
+          onClick={btnAction}
+          className="rounded text-white font-semibold bg-cyan-600 px-3 py-2"
+        >
+          Sign Up
+        </button>
+      </div>
+    </>
   )
 }
 
