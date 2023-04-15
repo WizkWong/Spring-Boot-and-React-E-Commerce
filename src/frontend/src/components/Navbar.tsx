66f5c2d3550649e40f2965732fb07e7f3cb98e56
WizkWong <chijianwong7@gmail.com>
2023-04-15 17:22:41 +0800

    add from tag and use Link component for Nav

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import search from "../assets/search.svg";
+import searchImg from "../assets/search.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const nav_category: string[] = [
@@ -11,23 +12,39 @@ const Navbar = () => {
   return (
     <nav>
       <div className="flex items-center bg-gray-300 py-3 text-xl">
-        <div className="flex-none mx-4">{import.meta.env.VITE_WEBSITE_TITLE}</div>
-        <div className="flex-1 flex item-center">
-          <input className="grow pl-2 py-2" placeholder="Search ..."></input>
-          <img
-            src={search}
-            alt="search"
-            className="flex-none bg-yellow-600 px-3 hover:cursor-pointer"
-          ></img>
+        <div className="flex-none mx-4">
+          {import.meta.env.VITE_WEBSITE_TITLE}
         </div>
+        <form method="GET" className="flex-1 flex item-center">
+          <input
+            className="grow pl-2 py-2"
+            type="test"
+            name="search"
+            placeholder="Search ..."
+          ></input>
+          <button className="flex-none">
+            <img
+              src={searchImg}
+              alt="search"
+              className="bg-yellow-600 min-h-full px-3"
+            ></img>
+          </button>
+        </form>
         <div className="flex-none flex items-center">
-          <a className="px-6 hover:cursor-pointer" href="/login">Login</a>
-          <a className="px-6 hover:cursor-pointer" href="/signup">Sign Up</a>
+          <Link to="login" className="mx-6">
+            Login
+          </Link>
+          <Link to="signup" className="mx-6">
+            Sign Up
+          </Link>
         </div>
       </div>
       <div className="flex items-center bg-gray-100 py-1">
         {nav_category.map((element, index) => (
-          <p key={index} className="px-8 hover:text-black hover:cursor-pointer text-gray-600 font-bold">
+          <p
+            key={index}
+            className="px-8 hover:text-black hover:cursor-pointer text-gray-600 font-bold"
+          >
             {element}
           </p>
         ))}
