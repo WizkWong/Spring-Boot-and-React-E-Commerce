8afa9375d0ac4dafcd0b048cb439d491bcd71139
WizkWong <chijianwong7@gmail.com>
2023-04-17 20:36:19 +0800

    add change password feature, and user validation and update will no longer validate and update password

--- src/main/java/com/example/SpringBootDemo/security/auth/AuthenticationController.java
+++ src/main/java/com/example/SpringBootDemo/security/auth/AuthenticationController.java
@@ -2,6 +2,8 @@ package com.example.SpringBootDemo.security.auth;
 
 import com.example.SpringBootDemo.customer.Customer;
 import com.example.SpringBootDemo.customer.CustomerDTO;
+import com.example.SpringBootDemo.user.RequestChangePassword;
+import com.example.SpringBootDemo.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody Customer customer) {
@@ -27,5 +30,10 @@ public class AuthenticationController {
     @GetMapping("/profile")
     public CustomerDTO getProfile(@RequestHeader(name = "Authorization") String token) {
         return authenticationService.getProfile(token);
+    }
+
+    @PostMapping("/changePassword")
+    public void changePassword(@RequestBody RequestChangePassword request) {
+        userService.changePassword(request);
     }
 }
