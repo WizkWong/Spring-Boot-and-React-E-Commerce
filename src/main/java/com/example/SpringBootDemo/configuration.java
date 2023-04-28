cfd638a2d148946ed806c93ecaa2a8936fa1d2cd
WizkWong <chijianwong7@gmail.com>
2023-04-28 18:53:25 +0800

    fixed the cors error by allowing all the Http methods

--- src/main/java/com/example/SpringBootDemo/configuration.java
+++ src/main/java/com/example/SpringBootDemo/configuration.java
@@ -22,7 +22,8 @@ public class configuration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");;
             }
         };
     }
